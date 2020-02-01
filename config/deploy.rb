a494eff08163daa09539a298f389e0152ffa810c
MihaelKeehl111 <aikawayuuya@icloud.com>
2020-02-01 18:41:30 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'freemarket_sample_61a'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com/MihaelKeehl111/freemarket_sample_61a.git'
+set :repo_url,  'git@github.com:MihaelKeehl111/freemarket_sample_61a.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
