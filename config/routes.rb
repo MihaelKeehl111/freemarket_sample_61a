adba8cfdd42f2fa6063265eaaf1fc8e08ebc4c08
mimoa-t <mimoa.toki@gmail.com>
2020-02-02 17:04:14 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "products#index"
   resources :products
+<<<<<<< Updated upstream
+=======
+  
+>>>>>>> Stashed changes
   resources :users do
     collection do
       get :logout
