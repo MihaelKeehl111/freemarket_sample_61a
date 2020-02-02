880547f148b844a3bd52733cac27602a9c65db15
mimoa-t <mimoa.toki@gmail.com>
2020-02-02 19:52:25 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "products#index"
-  
+
   resources :products
 
-  resources :users do
+  resources :users, only: [:index, :edit] do
     collection do
       get :logout
     end
