c37eda65b136290b7c4a284bfc4bf3f71b846363
MY725 <infinite.illusion25@gmail.com>
2020-02-09 17:49:43 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  resources :products, only: [:index]
+  resources :products, only: [:index, :new]
 
   resources :users, only: [:index, :edit] do
     collection do
