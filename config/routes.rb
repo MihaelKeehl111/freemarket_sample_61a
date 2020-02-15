8763b7d8b0c7b01f0c302518f3fe1b5781cb3d12
yoshitaro2019 <yomi_majoko@yahoo.co.jp>
2020-02-15 11:34:59 +0900

    view-products-detail
    ・途中保存

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  resources :products, only: [:index, :new]
+  resources :products, only: [:index, :new, :show]
 
   resources :users, only: [:index, :edit] do
     collection do
