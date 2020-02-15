bfca6f9c60563928284b1e1f0e5252598ec3a237
MihaelKeehl111 <aikawayuuya@icloud.com>
2020-02-15 19:10:15 +0900

    puroduct-buy-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root "products#index"
+  root "products#buy"
 
-  resources :products, only: [:index, :new]
+  resources :products, only: [:index, :new] do
+    collection do
+      get :buy
+    end
+  end  
+
 
   resources :users, only: [:index, :edit] do
     collection do
