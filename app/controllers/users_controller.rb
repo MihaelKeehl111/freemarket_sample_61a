c21db49a5850f45df2f8384b3679857a2f5eea54
mimoa-t <mimoa.toki@gmail.com>
2020-02-24 22:57:14 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
 
   def identification
     @address = Address.find_by(user_id: current_user.id)
+    # if @address.update(address_params)
+    #   redirect_to users_path
+    # else
+    #   render :identification
+    # end
   end
 
   def edit
@@ -36,4 +41,10 @@ class UsersController < ApplicationController
       card_attributes: [:id, :card_number, :expiration_month, :expiration_year, :security_code]
     )
   end
+
+  # def address_params
+  #   params.require(:addres).permit(
+  #     :postcode, :prefecture, :municipality, :address, :building
+  #   )
+  # end
 end
