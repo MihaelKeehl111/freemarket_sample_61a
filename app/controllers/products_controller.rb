a490d077c3470f03929627dd7ca80333f6a4809e
MY725 <infinite.illusion25@gmail.com>
2020-03-07 19:18:39 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_category, only: :index
+  before_action :set_current_user_products, only: [:exhibiting, :trading, :sold, :purchase, :purchased]
 
   def index
     @products = Product.order('created_at DESC')
@@ -20,23 +21,18 @@ class ProductsController < ApplicationController
   end
 
   def exhibiting
-    @products = current_user.products
   end
 
   def trading
-    @products = current_user.products
   end
 
   def sold
-    @products = current_user.products
   end
 
   def purchase
-    @products = current_user.products
   end
 
   def purchased
-    @products = current_user.products
   end
 
   def new
@@ -64,6 +60,10 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:image, :name, :description, :category_id, :size, :state_id, :delivery_charge_id, :delivery_method_id, :delivery_area_id, :delivery_date_id, :price).merge(user_id: current_user.id, status_id: 1)
+  end
+
+  def set_current_user_products
+    @products = current_user.products
   end
 
   def set_category
