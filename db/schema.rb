6c8ab02bd68db3ff0fe06462cbc8a70c4cec21c6
yoshitaro2019 <yomi_majoko@yahoo.co.jp>
2020-03-09 21:03:30 +0900

    schema.rbでのコンフリクト解消
    shema.rbでのコンフリクト解消のため、versionの数字が大きい方を残す

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20200227051716) do
+# ActiveRecord::Schema.define(version: 20200227051716) do
 
-# ActiveRecord::Schema.define(version: 20200301064135) do
+ActiveRecord::Schema.define(version: 20200301064135) do
 
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
