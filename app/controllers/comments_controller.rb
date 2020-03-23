e6b4ccc281557da9f129a65eec6a6842c370466f
mimoa-t <mimoa.toki@gmail.com>
2020-03-23 19:09:49 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment: comment_params[:comment], user_id: current_user.id, product_id: comment_params[:product_id])
-    redirect_to product_path(comment.product.id)
+    comment = current_user.comments.new(comment_params)
+    if comment.save
+      redirect_to product_path(comment.product.id)
+    else
+      redirect_to product_path(comment_params[:product_id])
+    end
   end
 
   def destroy
-    comment = Comment.find_by(id: params[:id], product_id: params[:product_id])
+    comment = Comment.find(params[:id])
     comment.destroy if comment.user_id == current_user.id
     if comment.destroy
       redirect_to product_path(comment.product_id)
