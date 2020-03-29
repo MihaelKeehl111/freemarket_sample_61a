564a5977843fb730fff0d1c68bbe5fb6abe80ab2
mimoa-t <mimoa.toki@gmail.com>
2020-03-29 15:08:16 +0900

    update

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,4 @@ class Image < ApplicationRecord
   belongs_to :product
   mount_uploader :image, ImageUploader
 
-  def set_first_image
-    images = Image.where(product_id: product.id)
-    product_image = images.first
-  end
 end
