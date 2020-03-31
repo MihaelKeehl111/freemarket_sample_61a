89e8a906ef1da734d43fff9ba30bcb59b53969fb
MihaelKeehl111 <aikawayuuya@icloud.com>
2020-03-31 16:59:02 +0900

    update

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -126,7 +126,7 @@ class SignupController < ApplicationController
     )
     @user.build_address(session[:address_attributes])
 
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp.api_key = Rails.application.secrets.payjp_secret_key
     if params['payjpToken'].blank?
       render '/signup/register_card'
     else
